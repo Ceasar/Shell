4d01104f2792ac458b6e8974f6dbd78c33d849fc
Ceasar Bautista <ceasar.bautista@adroll.com>
2015-08-14 14:25:02 -0700

    Indicate which command failed on failure

--- sh.py
+++ sh.py
@@ -5,5 +5,5 @@ def sh(command):
     """Run a command in the shell and capture the output."""
     try:
         return subprocess.check_output(command, shell=True)
-    except subprocess.CalledProcessError:
-        raise ValueError("command failed")
+    except subprocess.CalledProcessError as e:
+        raise ValueError("'%s' failed: %s" % (command, e))
